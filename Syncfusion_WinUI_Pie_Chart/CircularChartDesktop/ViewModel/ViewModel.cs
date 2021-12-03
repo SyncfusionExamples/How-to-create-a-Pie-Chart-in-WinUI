63cc62552b98149851e515f367be74230ef3ea84
AzureAD\TempKaruna <gayathri.manickam@syncfusion.com>
2021-12-03 14:16:05 +0530

    Update demo sample

---
+++ Syncfusion_WinUI_Pie_Chart/CircularChartDesktop/ViewModel/ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircularChartDesktop
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>()
+            {
+                new Model("Algeria", 28),
+                new Model("Australia", 14),
+                new Model("Bolivia", 31),
+                new Model("Cambodia", 77),
+                new Model("Canada", 19),
+            };
+        }
+    }
+}
